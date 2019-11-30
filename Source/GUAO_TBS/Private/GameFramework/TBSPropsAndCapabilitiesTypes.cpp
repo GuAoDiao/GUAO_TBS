c4d08e2d2986798e0cf4d4040999519e0bfee085
GuAoDiao <714896324@qq.com>
2019-11-30 21:34:18 +0800

    # 重新添加一个空项目, 旧有内容移动到分支old中

--- Source/GUAO_TBS/Private/GameFramework/TBSPropsAndCapabilitiesTypes.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "TBSPropsAndCapabilitiesTypes.h"
-
-const FGamePropsInfo FGamePropsInfo::EmptyProps{};
-const FConsumablesPropsInfo FConsumablesPropsInfo::EmptyConsumablesProps{};
-const FPropsShopInfo FPropsShopInfo::EmptyFPropsShopInfo{};
--- Source/GUAO_TBS/Private/UI/CombatLayoutWidget.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "CombatLayoutWidget.h"
-
-void UCombatLayoutWidget::InitializeGameLayoutWidget_Implementation()
-{
-	CombatManager = nullptr;
-}
